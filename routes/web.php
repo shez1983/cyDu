23627b61cb056b0b5bc06dc5d55602e96decfb08
shehzad <shehzad@aflete.com>
2020-10-08 12:16:58 +0100

    chore: adding LoaderIO verification

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Auth::routes(['register' => false]);
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('companies', 'CompaniesController');
 Route::resource('employees', 'EmployeesController');
+
+Route::get('/loaderio-7cd85db4d5a59f9bce6d58c0405d884d', function (){
+   return 'loaderio-7cd85db4d5a59f9bce6d58c0405d884d';
+});
